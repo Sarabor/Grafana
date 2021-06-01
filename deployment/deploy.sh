d516055fc1fe076f14a069c1ff363058b3ebc2dc
Sarabor <fleschm@outlook.de>
2021-06-01 11:39:17 +0200

    working test

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+
+: "${DASHBOARD:=${1:-}}"
+if [[ -z "${DASHBOARD}" ]]; then
+        echo 'Must provide DASHBOARD'
+        read -n1 -s -r -p $'\nPress any button to continue...\n' key
+        exit 1
+fi
+
 JSONNET_PATH=$grafonnet \
-jsonnet $1 > dashboard.json
+jsonnet "${DASHBOARD}" > dashboard.json
+
+
+
+resp=$(mktemp)
 
 payload="{\"dashboard\": $(jq . dashboard.json), \"overwrite\": true}"
-curl -X POST --insecure -H "Authorization: Bearer $AUTH" \
+resp=$(curl -X POST --insecure -H "Authorization: Bearer $AUTH" \
 -H "Content-Type: application/json" \
 -d "${payload}" \
-http://localhost:3000/api/dashboards/db
+http://localhost:4000/api/dashboards/db)
+
+
+if [[ $resp = *success* ]]
+then
+    echo $'\n''Dashboard successfully deployed!'
+else
+    echo $'\n''Something went wrong. Full Message:'
+    echo $resp
+fi
+
 
 read -n1 -s -r -p $'\nPress any button to continue...\n' key
---
+++ deployment/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+: "${DASHBOARD:=${1:-}}"
+: "${GRAFANA:=${2:-}}"
+if [[ -z "${DASHBOARD}" ]]; then
+        echo 'Must provide DASHBOARD'
+        read -n1 -s -r -p $'\nPress any button to continue...\n' key
+        exit 1
+fi
+if [[ -z "${GRAFANA:-}" ]]; then
+        echo 'Must provide GRAFANA'
+        read -n1 -s -r -p $'\nPress any button to continue...\n' key
+        exit 1
+fi
+
+dbjson=$(mktemp)
+jsonnet -J $grafonnet -J . "${DASHBOARD}" > "${dbjson}"
+
+json=$(mktemp)
+jq "{ \"dashboard\": ., \"expires\": 300 }" < "${dbjson}" > "${json}"
+
+resp=$(curl -X POST --insecure -H "Authorization: Bearer ${AUTH}" \
+-H 'Content-type: application/json' -H 'Accept: application/json' \
+"http://localhost:${GRAFANA}/api/snapshots" --data-binary "@${json}")
+
+ url=$(echo "${resp}" | jq -r ".url | sub(\"http://localhost:3000\"; \"http://localhost:${GRAFANA}\")")
+
+start "${url}"
+
+read -n1 -s -r -p $'\nPress any button to continue...\n' key
